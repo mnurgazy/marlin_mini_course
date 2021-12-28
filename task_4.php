481c9566589c1607dc4e57a27433bc2b24dfc301
Nurgazy <79838094+mnurgazy@users.noreply.github.com>
2021-12-28 22:59:30 +0600

    Add files via upload

---
+++ task_4.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>
+            Подготовительные задания к курсу
+        </title>
+        <meta name="description" content="Chartist.html">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+        <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+        <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+        <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+        <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
+        <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+    </head>
+    <body class="mod-bg-1 mod-nav-link ">
+        <main id="js-page-content" role="main" class="page-content">
+            <div class="col-md-6">
+                <div id="panel-1" class="panel">
+                    <div class="panel-hdr">
+                        <h2>
+                            Задание
+                        </h2>
+                        <div class="panel-toolbar">
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                        </div>
+                    </div>
+                    <?php
+                        $items = [
+                            [
+                                "title" => "My Tasks",
+                                "inline_block" => "130 / 500",
+                                "progress_bar" => "bg-fusion-400",
+                                "style" => "65%",
+                                "valuenow" => "65",
+                                "valuemin" => "0",
+                                "valuemax" => "100"
+                            ],
+                            [
+                                "title" => "Transfered",
+                                "inline_block" => "440 TB",
+                                "progress_bar" => "bg-success-500",
+                                "style" => "34%",
+                                "valuenow" => "34",
+                                "valuemin" => "0",
+                                "valuemax" => "100"
+                            ],
+                            [
+                                "title" => "Bugs Squashed",
+                                "inline_block" => "77%",
+                                "progress_bar" => "bg-info-400",
+                                "style" => "77%",
+                                "valuenow" => "77",
+                                "valuemin" => "0",
+                                "valuemax" => "100"
+                            ],
+                            [
+                                "title" => "User Testing",
+                                "inline_block" => "7 days",
+                                "progress_bar" => "bg-primary-300",
+                                "style" => "84%",
+                                "valuenow" => "84",
+                                "valuemin" => "0",
+                                "valuemax" => "100"
+                            ]
+                        ];
+                    ?>
+                    <div class="panel-container show">
+                        <div class="panel-content">
+                            <?php foreach ($items as $item):?>
+                                <div class="d-flex mt-2">
+                                    <?php echo $item['title'];?>
+                                    <span class="d-inline-block ml-auto"><?php echo $item['inline_block'];?></span>
+                                </div>                            
+                                <div class="progress progress-sm mb-3">
+                                    <div class="progress-bar <?php echo $item['progress_bar'];?>" role="progressbar" style="width: <?php echo $item['style'];?>" aria-valuenow="<?php echo $item['valuenow'];?>" aria-valuemin="<?php echo $item['valuemin'];?>" aria-valuemax="<?php echo $item['valuemax'];?>"></div>
+                                </div>
+                            <?php endforeach;?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
+
+        <script src="js/vendors.bundle.js"></script>
+        <script src="js/app.bundle.js"></script>
+        <script>
+            // default list filter
+            initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+            // custom response message
+            initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+        </script>
+    </body>
+</html>
