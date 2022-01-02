79bac710f7b06bc82865dd1b97b90b469adb12b0
Nurgazy <79838094+mnurgazy@users.noreply.github.com>
2022-01-03 01:56:15 +0600

    Update task_16_handler.php

--- task_16_handler.php
+++ task_16_handler.php
@@ -1,20 +1,29 @@
 <?php 
-$image = $_FILES['image'];
 
-$ext = pathinfo($image['name'], PATHINFO_EXTENSION);
-$name = uniqid() . "." . $ext;
-$path = "img/demo/gallery/";
-move_uploaded_file($image['tmp_name'], $path . $name);
-$imagelink = $path . $name;
-
-if (empty($image['name'])) {
-	header("Location: /task_16.php");
-	exit;
+function get_images () {
+	$pdo = new PDO ("mysql:host=localhost;dbname=my_project", "root", "");
+    $sql = "SELECT * FROM images";
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+    $images = $statement->fetchALL(PDO::FETCH_ASSOC);
+    return $images;
 }
-$pdo = new PDO ("mysql:host=localhost;dbname=my_project", "root", "");
 
-$sql = "INSERT INTO images (image) VALUES ('$imagelink')";
-$statement = $pdo->prepare($sql);
-$statement->execute();
 
-header("Location: /task_16.php");
+function upload_image ($image) {
+	$ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+	$name = uniqid() . "." . $ext;
+	$path = "img/demo/gallery/";
+	move_uploaded_file($image['tmp_name'], $path . $name);
+	$imagelink = $path . $name;
+
+	if (empty($image['name'])) {
+		header("Location: /task_16.php");
+		exit;
+	}
+	$pdo = new PDO ("mysql:host=localhost;dbname=my_project", "root", "");
+
+	$sql = "INSERT INTO images (image) VALUES ('$imagelink')";
+	$statement = $pdo->prepare($sql);
+	$statement->execute();
+}
