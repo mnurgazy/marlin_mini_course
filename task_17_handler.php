a3e201aae9c4d9a4fbb1bd8569ec1016aad665dd
Nurgazy <79838094+mnurgazy@users.noreply.github.com>
2022-01-05 10:56:44 +0600

    Update task_17_handler.php

--- task_17_handler.php
+++ task_17_handler.php
@@ -25,10 +25,6 @@ function upload_image ($image) {
 	move_uploaded_file($image['tmp_name'], $path . $name);
 	$imagelink = $path . $name;
 
-	if (empty($image['name'])) {
-		header("Location: /task_17.php");
-		exit;
-	}
 	$pdo = new PDO ("mysql:host=localhost;dbname=my_project", "root", "");
 
 	$sql = "INSERT INTO images (image) VALUES ('$imagelink')";
