19a1bafa103397911ef2fe283a32131e9f797e9e
Nurgazy <79838094+mnurgazy@users.noreply.github.com>
2022-01-09 14:03:38 +0600

    Update task_18_upload.php

--- task_18_upload.php
+++ task_18_upload.php
@@ -2,6 +2,10 @@
 require "task_18_handler.php";
 $image = $_FILES['image'];
 
-upload_image ($image);
+$total_files = count($image['name']);
+
+for ($key=0; $key < $total_files; $key++) {
+	upload_image ($image['name'][$key], $image['tmp_name'][$key]);
+}
 
 header("Location: /task_18.php");
